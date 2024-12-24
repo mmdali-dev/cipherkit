6743fca91ee06d85659090e4334ff91ec4a89bdc
Mohammad Ali <core.my.activity@gmail.com>
2024-12-24 16:17:00 +0330

    fix : config aes256 and rsa for best perforamnce and easy usage

--- main.go
+++ main.go
@@ -1,17 +1,5 @@
 package main
 
-import (
-	"fmt"
-
-	"github.com/mmdali-dev/cipherkit/lib/aes256"
-)
-
 func main() {
-	key, _ := aes256.LoadKey("a")
-	fmt.Println(key)
-	bkey, _ := aes256.StringToKey(key)
-	encoded, _ := aes256.Encrypt(bkey, []byte("mohammad"))
-	fmt.Println(encoded)
-	decoded, _ := aes256.Decrypt(bkey, encoded)
-	fmt.Println(string(decoded))
+
 }
